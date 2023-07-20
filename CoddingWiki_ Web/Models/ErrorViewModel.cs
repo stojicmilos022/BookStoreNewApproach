bdb561ad91670c8c4fac00f345809f5985ce9d5e
stojicmilos022 <stojic.milos@gmail.com>
2023-07-20 09:46:04 +0200

    Add project files.

---
+++ CoddingWiki_ Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoddingWiki__Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
